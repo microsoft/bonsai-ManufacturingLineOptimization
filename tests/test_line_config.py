8e5e85cda290678d0efbc803a9d019c08ff669b5
HosseinHeris <hossein.khadivi@microsoft.com>
2021-03-31 10:40:53 -0700

    bonsai integration

---
+++ tests/test_line_config.py
@@ -0,0 +1,22 @@
+'''
+A set of tests to insure that line configuration makes sense
+
+(1) connectivity of the line (2) existance of at least one source and one sink (3) no dangling machine/conveyor  
+
+'''
+
+from ../sim/line_config import adj, con_balance, con_join 
+
+def test_format():
+
+    assert 
+
+def test_line_con():
+
+    assert 
+
+def test_line_con():
+
+    assert 
+    
+
