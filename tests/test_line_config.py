49b4f10e5488a640edb233ca5d59e52819be596c
HosseinHeris <hossein.khadivi@microsoft.com>
2021-04-07 10:36:48 -0700

    Few tests added, git ingnore added to ignore pycache

--- tests/test_line_config.py
+++ tests/test_line_config.py
@@ -4,19 +4,28 @@ A set of tests to insure that line configuration makes sense
 (1) connectivity of the line (2) existance of at least one source and one sink (3) no dangling machine/conveyor  
 
 '''
+import os 
+# from pathlib import Path
+# p= Path(os.getcwd())
+# os.chdir(p.parent)
+from line_config import adj, con_balance, con_join 
 
-from ../sim/line_config import adj, con_balance, con_join 
+def test_adj_format():
+    assert type(adj) == dict
+    for machine in adj.keys():
+        assert type(adj[machine]) == tuple 
 
-def test_format():
+def test_line_con_format():
+    assert type(con_balance) == list
+    if con_balance:
+        assert type(con_balance[0]) == tuple
 
-    assert 
+def test_line_con_join():
+    assert type(con_join) == list
+    if con_join:
+        assert type(con_join[0]) == tuple
 
-def test_line_con():
-
-    assert 
-
-def test_line_con():
-
-    assert 
+def test_adj_machine_name_unique():
+    
     
 
